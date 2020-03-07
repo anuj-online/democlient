60f6483eee0eb11e2528568a7d772369a640abd5
anuj <anujbookman@gmail.com>
2020-03-07 12:21:14 +0100

    added url and uri properties to application.properties

--- src/main/java/com/example/demo/controllers/ClientController.java
+++ src/main/java/com/example/demo/controllers/ClientController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +15,17 @@ import java.time.Duration;
 @Slf4j
 public class ClientController {
 
+    @Value("${demo.server.url}")
+    private String endpoint;
 
+    @Value("${demo.server.uri}")
+    private String uri;
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<DataObject> callServer(){
 
-        WebClient webClient = WebClient.create("http://localhost:8081");
+        WebClient webClient = WebClient.create(endpoint);
 
-        Flux<DataObject> dataObjectFlux = webClient.get().uri("/all/1").retrieve().bodyToFlux(DataObject.class);
+        Flux<DataObject> dataObjectFlux = webClient.get().uri(uri).retrieve().bodyToFlux(DataObject.class);
 
         dataObjectFlux.delayElements(Duration.ofSeconds(1L)).doOnNext(
                 x -> log.info("Received : {}", x)
